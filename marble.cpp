e0f1155b24fca2930ef79ea4e92f5b5836e107e6
sandrageo16 <sandrageo16@gmail.com>
2017-12-22 03:38:25 +0100

    playground and marble movement combined
    Recognition of pit holes has to be modified.
    Also the radius of the marble has to be considered when calculating the new position.

--- marble.cpp
+++ marble.cpp
@@ -3,68 +3,105 @@
 #include <stdio.h>
 #include <time.h>
 #include <mraa.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
 #include "OLEDDisplay.h"
 
 using std::chrono::steady_clock;
 using namespace GFX;
-
-void testBasicCommon(OLEDDisplay &lcd, color_t bg, color_t fg) {
-	lcd.setBgColor(bg);
-	lcd.setDrawColor(fg);
+using namespace std;
+int display [128][128];
+void getInput(){
+	int i,j;
+	i=0;
+	string line;
+	ifstream myfile("Array.txt");
+	if (myfile.is_open()){
+		for (i=0; i<128; i++){
+			getline (myfile, line);
+			for (j=0; j<128; j++){
+				display[j][i]= line[j];
+			}
+		}
+	myfile.close();
+	}
+	else cout<< "Ãœnable to open file \n";
+	printf("test3 \n");
+}
+void lcd_display(OLEDDisplay &lcd){
 	lcd.clearScreen();
-	lcd.enable();
+	int k,l;
+	for (l=0; l<128; l++){
+		for (k=0; k<128; k++){
+			if (display[l][k] =='1'){
+				lcd.drawPixel(l,k);
+			}
+		}
+	}
+			
+	lcd.setCursor(36, 17);
+	lcd.setTextColor(COLOR_WHITE);
+	lcd.print("LABRYNTH");
+	lcd.drawCircle(111, 119, 4);
+	lcd.fillCircle(111, 119, 4);
+	lcd.drawCircle(31, 106, 4);
+	lcd.fillCircle(31, 106, 4);
+	lcd.drawCircle(79, 87, 4);
+	lcd.fillCircle(79, 87, 4);
 
-	lcd.drawRect(1,1, 1, 1);
+	lcd.drawCircle(119, 111, 4);
+	lcd.fillCircle(119, 111, 4);	
+	lcd.drawCircle(103, 31, 4);
+	lcd.fillCircle(103, 31, 4);	
+	lcd.drawCircle(87, 79, 4);
+	lcd.fillCircle(87, 79, 4);
 
-	lcd.flush();
-	std::this_thread::sleep_for(std::chrono::seconds(5));
+	lcd.drawCircle(7, 111, 4);
+	lcd.fillCircle(7, 111, 4);	
+	lcd.drawCircle(23, 31, 4);
+	lcd.fillCircle(23, 31, 4);	
+	lcd.drawCircle(39, 79, 4);
+	lcd.fillCircle(39, 79, 4);
+	
 }
 
-void testBasic1() {
-	OLEDDisplay lcd(128, 128);
-	testBasicCommon(lcd, COLOR_WHITE, COLOR_BLACK);
+void end_game(OLEDDisplay &lcd){
+	while(1){
+		lcd_display(lcd);
+		lcd.setCursor(36, 33);
+		lcd.setTextColor(COLOR_WHITE);
+		lcd.print("GAME OVER");
+		lcd.flush();
+	
+}	
 }
-
-void testBasic2() {
-	OLEDDisplay lcd(128, 128);
-	testBasicCommon(lcd, COLOR_BLACK, COLOR_WHITE);
-}
-
 void testFPS() {
 	OLEDDisplay lcd(128, 128);
 	lcd.enable();
-
-	lcd.setBgColor(COLOR_WHITE);
-	lcd.setDrawColor(COLOR_BLACK);
+	lcd.setBgColor(COLOR_BLACK);
+	lcd.setDrawColor(COLOR_WHITE);
+	lcd.setTextColor(COLOR_WHITE);
+	getInput();;
 
 	steady_clock::time_point lastTime = steady_clock::now();
 	uint32_t lastFC = lcd.getFrameCounter();
 
-	const int radius = 4;
+	const int radius = 2;
 	const int minX = radius;
 	const int maxX = lcd.getWidth() - radius;
 	const int maxY = lcd.getHeight() - radius;
-/*
-	float posx0 = 0;
-	float posy0 = 0;
-	float posy1= 5;
-	float posx1 = 5;
-	float speedx = 0.5;
-	float speedy = 0.5;
-	float accely = 0.11;
-*/
 
 
-mraa::I2c i2c(0);
-i2c.address(0x68);
+	mraa::I2c i2c(0);
+	i2c.address(0x68);
 
-i2c.writeReg(0x6B, 0x80);
-usleep(100*1000);
-
-
-	float x0 = 64;
+	i2c.writeReg(0x6B, 0x80);
+	usleep(100*1000);
+	
+	float x0 = 2;
 	float x1 = x0;
-	float y0 = 64;
+	float y0 = 125;
 	float y1 = y0;
 
 	float xnew0 = 0;
@@ -80,18 +117,15 @@ usleep(100*1000);
 	float accx = 0;
 	float accy = 0;
 	
-	float dt = 0.5;
+	float dt = 0.2;
 	float friction = 0.98;
-
-	//	while(1);
-
-	lcd.drawLine(100,0,100,100);
-	lcd.drawRect(10,10,10,10);
 	
 	usleep(100000);
 	i2c.writeReg(0x6B, 0x00);
-	while (true) 
-	{
+	while (true) {
+		lcd_display(lcd);
+		lcd.flush();
+
 	//read raw accelerometer values
 	uint8_t acc_raw[6];
 	int len =i2c.readBytesReg(0x3B, acc_raw,6);
@@ -107,51 +141,26 @@ usleep(100*1000);
 		accx = ax_raw / -16384.0f;
 		accy = ay_raw / 16384.0f;
 		float az = az_raw / 16384.0f;
-/*		
-		printf("X: %f\n", ax);
-		printf("Y: %f\n", ay);
-		printf("Z: %f\n", az);
-*/	}
-	
-
-
+	}
 		speedx1 = (speedx + (accx*dt)) * friction;
 		speedy1 = (speedy + (accy*dt)) * friction;
 
 		xnew0 = xnew1 = x0 + (speedx1*dt);
 		ynew0 = ynew1 = y0 + (speedy1*dt);
 
-		//usleep(1000000);
 		lcd.clearScreen();
-		
-	/*
-	lcd.drawLine(100,0,100,100);	
-		if((xnew1 >= 100) && (ynew1>= 0 || ynew1 <= 100))
-		{
-			printf("\nINside if\n");
-			xnew1 = 98;
+		if ((int(xnew0)== 111) & (int(ynew0)== 119)){ end_game(lcd);}
+		if ((int(xnew0)== 31) & (int(ynew0)== 106)){ end_game(lcd);}
+		if ((int(xnew0)== 79) & (int(ynew0)== 87)){ end_game(lcd);}
+		if (display[int(xnew0)][int(ynew0)]=='1'){ xnew0 = x0; ynew0 = y0;}
+        if (xnew0 < 2) { xnew0=2; speedx1=0;}
+        if (xnew0 > 125) { xnew0=125; speedx1=0;}
+        if (ynew0 < 2) { ynew0=2; speedy1=0;}
+        if (ynew0 > 125) { ynew0=125; speedy1=0;}
 
-			lcd.drawCircle(xnew1,ynew0,2);
-		printf("in while, x0: %f, x1: %f, y0: %f, y1: %f,speedx = %f,speedy = %f,accx = %f,accy = %f\n",x0,xnew1,ynew0,y1,speedx1,speedy1,accx,accy);
-
-
-		}
-		else
-		{
-					lcd.drawCircle(xnew1,ynew1,2);
-		printf("in while, x0: %f, x1: %f, y0: %f, y1: %f,speedx = %f,speedy = %f,accx = %f,accy = %f\n",xnew0,xnew1,ynew0,ynew1,speedx1,speedy1,accx,accy);
-
-		
-		}
-         */
          
-         if (xnew0 < 2) { xnew0=2; speedx1=0;}
-         if (xnew0 > 125) { xnew0=125; speedx1=0;}
-         if (ynew0 < 2) { ynew0=2; speedy1=0;}
-         if (ynew0 > 125) { ynew0=125; speedy1=0;}
-         
-		printf("in while, x0: %f, x1: %f, y0: %f, y1: %f,speedx = %f,speedy = %f,accx = %f,accy = %f\n",xnew0,xnew1,ynew0,ynew1,speedx1,speedy1,accx,accy);
-			lcd.drawCircle(xnew0,ynew0,2);
+		//printf("in while, x0: %f, x1: %f, y0: %f, y1: %f,speedx = %f,speedy = %f,accx = %f,accy = %f\n",xnew0,xnew1,ynew0,ynew1,speedx1,speedy1,accx,accy);
+		lcd.drawCircle(xnew0,ynew0,radius);
 
 		speedx = speedx1;
 		speedy = speedy1;
@@ -159,7 +168,6 @@ usleep(100*1000);
 		y0 = ynew0;
 	
 		lcd.flush();
-
 		
 		steady_clock::time_point curTime = steady_clock::now();
 		if (curTime - lastTime >= std::chrono::seconds(1)) {
@@ -173,8 +181,6 @@ usleep(100*1000);
 
 int main(void) {
 	printf("Starting\n");
-//	testBasic1();
-//	testBasic2();
 	testFPS();
 	return 0;
 }
