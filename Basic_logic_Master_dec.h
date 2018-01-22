0407ffbe0cf6e7d47db7be504d6313a642a34a0f
Sumit S Shinde <sumitshinde197@gmail.com>
2018-01-23 00:21:03 +0100

    Very basic idea about deciding the master....
    Need a 1-2 days to write in systematic way

---
+++ Basic_logic_Master_dec.h
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <chrono>
+
+
+int {X1=0;Y1=0;X2=0;Y2=0;X3=0;Y3=0;Xi=0;Yi=0}
+char MasterAssignmentDone==0;
+
+bool decideMaster
+bool set_Pi01,set_Pi02,set_Pi03
+int main(void)
+
+ {
+	while(decideMaster==1)
+	{
+
+	#receive[other_pi_parameters]  P1,P2,P3   (P1= sum of the temp and random no between 0 to 100)
+	
+	if(P1>P2 && P1>P3)
+	{
+ 	 set_Pi01==1;		  #P1 is Master
+	 set_Pi02==0;
+	 set_Pi03==0;
+	
+	}
+	if(P2>P1 && P2>P3)
+	{
+	  set_Pi01==0;		
+	  set_Pi02==1; 		   #P1 is Master
+	  set_Pi03==0;
+	}
+
+	if(P3>P1 && P3>P2)
+	{
+	  set_Pi01==0;		
+	  set_Pi02==0; 		
+	  set_Pi03==1;		   #P1 is Master
+	}
+	  decideMaster=0 ;     #Master Pi has been set
+	}
+
+	
+	while(set_Pi01==1)
+	
+	{
+		
+		#RUN MASTER TASK FOR Pi01 
+		#DECEDING WINNER
+		
+		
+			
+	if (master==True) {
+
+		if (X1 >= 60 && X1 <= 70 Y1 >= 60 && Y1 <= 70 ) 
+		{
+			send="STOP"
+			display("Player_01 is a Winner")
+		}
+		if (X2 >= 60 && X2 <= 70 Y2 >= 60 && Y2 <= 70 ) 
+		{
+			send="STOP"
+			display("Player_02 is a Winner")
+		}
+		if (X3 >= 60 && X3 <= 70 Y3 >= 60 && Y3 <= 70 )  
+		{
+			send="STOP"
+			display("Player_03 is a Winner")	
+
+		}
+
+ 		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
